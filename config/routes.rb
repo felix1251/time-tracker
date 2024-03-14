6b1f5770f2c907c131ba24b9bc831545cdb05de3
felix1251 <domhack123@gmail.com>
2024-03-14 16:46:06 +0800

    add & setup devise authentication gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -8,6 +9,21 @@ end
 # == Route Map
 #
 #                                   Prefix Verb   URI Pattern                                                                                       Controller#Action
+#                         new_user_session GET    /users/sign_in(.:format)                                                                          devise/sessions#new
+#                             user_session POST   /users/sign_in(.:format)                                                                          devise/sessions#create
+#                     destroy_user_session DELETE /users/sign_out(.:format)                                                                         devise/sessions#destroy
+#                        new_user_password GET    /users/password/new(.:format)                                                                     devise/passwords#new
+#                       edit_user_password GET    /users/password/edit(.:format)                                                                    devise/passwords#edit
+#                            user_password PATCH  /users/password(.:format)                                                                         devise/passwords#update
+#                                          PUT    /users/password(.:format)                                                                         devise/passwords#update
+#                                          POST   /users/password(.:format)                                                                         devise/passwords#create
+#                 cancel_user_registration GET    /users/cancel(.:format)                                                                           devise/registrations#cancel
+#                    new_user_registration GET    /users/sign_up(.:format)                                                                          devise/registrations#new
+#                   edit_user_registration GET    /users/edit(.:format)                                                                             devise/registrations#edit
+#                        user_registration PATCH  /users(.:format)                                                                                  devise/registrations#update
+#                                          PUT    /users(.:format)                                                                                  devise/registrations#update
+#                                          DELETE /users(.:format)                                                                                  devise/registrations#destroy
+#                                          POST   /users(.:format)                                                                                  devise/registrations#create
 #         turbo_recede_historical_location GET    /recede_historical_location(.:format)                                                             turbo/native/navigation#recede
 #         turbo_resume_historical_location GET    /resume_historical_location(.:format)                                                             turbo/native/navigation#resume
 #        turbo_refresh_historical_location GET    /refresh_historical_location(.:format)                                                            turbo/native/navigation#refresh
